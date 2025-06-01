6a7e9984a30c23af4ce01b82c870f5fe3b5f25b8
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2025-06-01 15:12:18 +0200

    Fix units

--- do_ensure_data.sh
+++ do_ensure_data.sh
@@ -22,4 +22,4 @@ fi
 
 endtime=$(date +%s)
 
-echo "Total repo size: " $(du -sh . | awk '{print int($1/(1024*1024*1024)), "GB"}') " updated in " $((endtime-starttime)) " seconds." 
+echo "Total repo size: " $(du -s . | awk '{print int($1/(1024*1024)), "GB"}') " updated in " $((endtime-starttime)) " seconds." 
