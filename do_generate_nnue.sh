9d3c0ed23143824e0cf4f75bd27d882b2df76404
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2025-06-01 23:30:31 +0200

    Some more refactoring (#14)
    * Some more refactoring
    
    * Fixes

---
+++ do_generate_nnue.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+ROOTDIR=$1
+TARGET=$2
+
+
+LASTCKPT=$ROOTDIR/lightning_logs/version_0/checkpoints/last.ckpt
+LASTNNUE=$ROOTDIR/lightning_logs/version_0/checkpoints/last.nnue
+
+FTOPTDATA=/workspace/data/official-stockfish/master-binpacks/test80-2022-08-aug-16tb7p.v6-dd.min.binpack
+
+
+# TODO: --ft_optimize (too slow for testing)
+
+export OMP_NUM_THREADS=8
+python serialize.py $LASTCKPT $LASTNNUE --features=HalfKAv2_hm^ --ft_compression=leb128 --ft_optimize_count=1000000 --ft_optimize_data=$FTOPTDATA
+
+NNUESHA=$(sha256sum $LASTNNUE | cut -c1-12)
+echo "copying $LASTNNUE to nn-$NNUESHA.nnue"
+mkdir -p $TARGET
+cp $LASTNNUE $TARGET/nn-$NNUESHA.nnue
+
