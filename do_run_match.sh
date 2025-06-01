49cf10ebb9dcf91aeb949fdaf7f8b7cf0a6610be
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2025-06-01 19:38:34 +0200

    reorder fastchess options

--- do_run_match.sh
+++ do_run_match.sh
@@ -20,8 +20,8 @@ cd $MATCHDIR
 
 /workspace/fastchess/fastchess -rounds $ROUNDS -games 2 -repeat -srand 42  -concurrency 280 \
                                -openings file=/workspace/data/UHO_Lichess_4852_v1.epd format=epd order=random \
-                               -each proto=uci option.Threads=1 option.Hash=16 tc=10+0.1 \
                                -ratinginterval 280 -report penta=true -pgnout file=match.pgn \
                                -engine name=master cmd=/workspace/Stockfish/src/stockfish \
                                $ENGINES \
+                               -each proto=uci option.Threads=1 option.Hash=16 tc=10+0.1 \
                                | tee fastchess.out | grep -v "Started game" | grep -v "Started game"
