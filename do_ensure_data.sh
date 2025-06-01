000d9eb6a1f3a4d7756b0d4a41547bf697bd557e
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2025-06-01 15:46:07 +0200

    Official-master binpacks (#10)
    * Also get the official-stockfish binpacks in place
    
    * Add timing info, use data

--- do_ensure_data.sh
+++ do_ensure_data.sh
@@ -4,6 +4,8 @@ export BASE=$1
 export OWNER=$2
 export REPO=$3
 
+starttime=$(date +%s)
+
 export GIT_LFS_CONCURRENT_TRANSFERS=8
 
 mkdir -p $BASE/$OWNER
@@ -15,4 +17,9 @@ if [ -f $REPO/.git/config ]; then
 else
   rm -f $REPO
   git clone https://huggingface.co/datasets/$OWNER/$REPO
+  cd $REPO
 fi
+
+endtime=$(date +%s)
+
+echo "Total repo size: " $(du -s . | awk '{print int($1/(1024*1024)), "GB"}') " updated in " $((endtime-starttime)) " seconds." 
