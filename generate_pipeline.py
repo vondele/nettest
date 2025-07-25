5894bec8578f01ef95bb19220160679fe3bf581e
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2025-07-25 12:31:28 +0200

    Further small cleanups

--- generate_pipeline.py
+++ generate_pipeline.py
@@ -89,20 +89,20 @@ def workspace_status(recipe, workspace_dir):
     return
 
 
-def start_yaml():
+def start_ci_yaml():
     """
     Needed header for the CI pipeline
     """
-    yaml_out = dict()
-    yaml_out["include"] = [
+    ci_yaml_out = dict()
+    ci_yaml_out["include"] = [
         {
             "remote": "https://gitlab.com/cscs-ci/recipes/-/raw/master/templates/v2/.ci-ext.yml"
         }
     ]
-    return yaml_out
+    return ci_yaml_out
 
 
-def generate_stages(recipe, yaml_out):
+def generate_stages(recipe, ci_yaml_out):
     """
     List the needed stages in the recipe.
     Will skip training steps that are already in  Final stage.
@@ -123,7 +123,7 @@ def generate_stages(recipe, yaml_out):
     if "testing" in recipe:
         stages.append("testing")
 
-    yaml_out["stages"] = stages
+    ci_yaml_out["stages"] = stages
 
     return
 
@@ -148,7 +148,7 @@ def generate_job_base():
     return job
 
 
-def generate_ensure_data(recipe, workspace_dir, yaml_out, shell_out):
+def generate_ensure_data(recipe, workspace_dir, ci_yaml_out, shell_out):
     """
     Extract all datasets from the training steps, as a set of all needed Huggingface owner/repo tuples.
     """
@@ -176,12 +176,12 @@ def generate_ensure_data(recipe, workspace_dir, yaml_out, shell_out):
 
     shell_out += job["script"]
 
-    yaml_out["ensureDataJob"] = job
+    ci_yaml_out["ensureDataJob"] = job
     return
 
 
 def generate_training_stages(
-    recipe, workspace_dir, ci_project_dir, yaml_out, shell_out
+    recipe, workspace_dir, ci_project_dir, ci_yaml_out, shell_out
 ):
     """
     Generate training stages, essentially just pointing out the current step sha and the one of the previous run.
@@ -215,14 +215,14 @@ def generate_training_stages(
 
             job["artifacts"] = {"expire_in": "1 month", "paths": [f"step_{this_sha}"]}
 
-            yaml_out[stage_name + "Job"] = job
+            ci_yaml_out[stage_name + "Job"] = job
 
         previous_sha = this_sha
 
     return
 
 
-def generate_testing_stage(recipe, workspace_dir, yaml_out, shell_out):
+def generate_testing_stage(recipe, workspace_dir, ci_yaml_out, shell_out):
     """
     Generate the testing stage
     """
@@ -246,7 +246,7 @@ def generate_testing_stage(recipe, workspace_dir, yaml_out, shell_out):
             job["script"].append(task)
 
     shell_out += job["script"]
-    yaml_out["testingJob"] = job
+    ci_yaml_out["testingJob"] = job
 
     return
 
@@ -257,7 +257,7 @@ def parse_recipe(recipe, workspace_dir, ci_project_dir):
     """
 
     # ci yaml header
-    yaml_out = start_yaml()
+    ci_yaml_out = start_ci_yaml()
     shell_out = ["#!/bin/bash", "", "set -ex", ""]
 
     # insert shas that uniquely identify each step based on the full history of the training recipe
@@ -267,20 +267,20 @@ def parse_recipe(recipe, workspace_dir, ci_project_dir):
     workspace_status(recipe, workspace_dir)
 
     # generate the stage names / stages
-    generate_stages(recipe, yaml_out)
+    generate_stages(recipe, ci_yaml_out)
 
     # generate the ensureData stages
-    generate_ensure_data(recipe, workspace_dir, yaml_out, shell_out)
+    generate_ensure_data(recipe, workspace_dir, ci_yaml_out, shell_out)
 
     # tricky bit ... generate the training stages
     generate_training_stages(
-        recipe, workspace_dir, ci_project_dir, yaml_out, shell_out
+        recipe, workspace_dir, ci_project_dir, ci_yaml_out, shell_out
     )
 
     # generate the match stage
-    generate_testing_stage(recipe, workspace_dir, yaml_out, shell_out)
+    generate_testing_stage(recipe, workspace_dir, ci_yaml_out, shell_out)
 
-    return yaml_out, shell_out
+    return ci_yaml_out, shell_out
 
 
 if __name__ == "__main__":
@@ -305,11 +305,11 @@ if __name__ == "__main__":
     with open(input_file) as f:
         recipe = yaml.safe_load(f)
 
-    yaml_out, shell_out = parse_recipe(recipe, workspace_dir, ci_project_dir)
+    ci_yaml_out, shell_out = parse_recipe(recipe, workspace_dir, ci_project_dir)
 
     print("Resulting pipeline: ", Path(output_file))
     with Path(output_file).open(mode="w", encoding="utf-8") as f:
-        yaml.dump(yaml_out, f, Dumper=MyDumper, default_flow_style=False, width=300)
+        yaml.dump(ci_yaml_out, f, Dumper=MyDumper, default_flow_style=False, width=300)
 
     print("Resulting shell script: ", Path(output_file).with_suffix(".sh"))
     with Path(output_file).with_suffix(".sh").open(mode="w", encoding="utf-8") as f:
