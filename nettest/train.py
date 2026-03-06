857d2fcbe1552475aa5cf2d604fa7e38e9160445
Tony Wang <35334345+TonyCongqianWang@users.noreply.github.com>
2026-03-06 19:59:45 +0100

    ddp_fix
    fixed torchrun command

--- nettest/train.py
+++ nettest/train.py
@@ -130,7 +130,7 @@ def run_trainer(environment, current_sha, previous_sha, run, nnue_pytorch_dir):
 
     num_gpus = len([d for d in devices.split(",") if d.strip()])
     nproc = max(1, num_gpus)
-    cmd += ["torchrun", f"--nproc-per-node={nproc}", "-u", "train.py"]
+    cmd += ["PYTHONUNBUFFERED=1", "torchrun", f"--nproc-per-node={nproc}", "train.py"]
 
     for binpack in run["binpacks"]:
         cmd.append(str(data_dir / binpack))
